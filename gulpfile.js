6277842ad99b8c9d312d7b6271c40040e0e42143
Alcino <alcinodagrsca@gmail.com>
2018-06-05 18:35:56 +0200

    Início do Projecto com SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
-var autoprefixer = require('autoprefixer');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('autoprefixer');
+var atImport = require('postcss-import');
 
-gulp.task('html', function() {
-  gulp.watch('../app/*.html');
-});
 
-gulp.task('styles', function() {
-
+gulp.task('html'), function () {
+  gulp.watch('./app/*.html');
+}
+gulp.task('sass', function() {
+  return gulp.src('./app/assets/styles/**/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(gulp.dest('./app/temp/styles'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 // Static Server + watching scss/html files
@@ -16,8 +23,8 @@ gulp.task('watch', function() {
         server: "./app"
     });
 
-    gulp.watch('./app/assets/styles/**/*.css', ['styles']);
-    gulp.watch('./app/index.html', ['html']).on('change', browserSync.reload);
+    gulp.watch('./app/assets/styles/**/*.scss', ['sass']);
+    gulp.watch('./app/*.html', ['html']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['sass', 'watch']);
